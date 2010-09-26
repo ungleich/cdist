a578fa37e3791c2985bc7f9e6718d3572254cf25
Nico Schottelius <nico@kr.ethz.ch>
2010-09-26 14:02:39 +0200

    more ideas
    Signed-off-by: Nico Schottelius <nico@kr.ethz.ch>

---
+++ conf/modules/efsh/base.sh
@@ -0,0 +1,6 @@
+efsh_base()
+{
+   efsh_servicedir="/home/services"
+   efsh_userdir="/home/users"
+   efsh_groupdir="/home/groups"
+}
---
+++ conf/modules/efsh/test.sh
@@ -0,0 +1,5 @@
+. ./base.sh
+
+echo $efsh_userdir
+efsh
+echo $efsh_userdir
