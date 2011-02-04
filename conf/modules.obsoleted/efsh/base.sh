97a70cfc8b0169d897043a6b8750c94e158f4354
Nico Schottelius <nico@kr.ethz.ch>
2011-02-04 23:05:53 +0100

    [ARCHIVE] Remove example modules containing collectd/efsh/nginx/postgres code
    Signed-off-by: Nico Schottelius <nico@kr.ethz.ch>

--- conf/modules.obsoleted/efsh/base.sh
+++
@@ -1,6 +0,0 @@
-efsh_base()
-{
-   efsh_servicedir="/home/services"
-   efsh_userdir="/home/users"
-   efsh_groupdir="/home/groups"
-}
--- conf/modules.obsoleted/efsh/test.sh
+++
@@ -1,5 +0,0 @@
-. ./base.sh
-
-echo $efsh_userdir
-efsh
-echo $efsh_userdir
