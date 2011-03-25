4d33e524a122745c8d62eb0015cd4b9ef46d4092
Nico Schottelius <nico@kr.ethz.ch>
2011-03-25 20:56:25 +0100

    begin to migrate makefile to shell script
    Signed-off-by: Nico Schottelius <nico@kr.ethz.ch>

--- build.sh
+++ build.sh
@@ -1,7 +1,27 @@
-# General
-PREFIX=/usr
-BINDIR=$(PREFIX)/bin
-MANDIR=$(PREFIX)/share/man
+#!/bin/sh
+#
+# 2011 Nico Schottelius (nico-cdist at schottelius.org)
+#
+# This file is part of cdist.
+#
+# cdist is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# cdist is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with cdist. If not, see <http://www.gnu.org/licenses/>.
+#
+#
+# Push a directory to a target, both sides have the same name (i.e. explorers)
+# or
+# Pull a directory from a target, both sides have the same name (i.e. explorers)
+#
 
 # Manpage and HTML
 A2XM=a2x -f manpage --no-xmllint
@@ -17,7 +37,6 @@ MANDIR=doc/man
 
 MAN1DSTDIR=$(MANDIR)/man1
 MAN7DSTDIR=$(MANDIR)/man7
-MANHTMLDIR=$(MANDIR)/html
 MANOUTDIRS=$(MAN1DSTDIR) $(MAN7DSTDIR)
 
 MAN1SRC=                        				 	\
