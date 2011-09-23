211212e079fb1582658b4448e0c1ac2c0efdf0d1
Nico Schottelius <nico@kr.ethz.ch>
2011-09-23 16:01:24 +0200

    test template
    Signed-off-by: Nico Schottelius <nico@kr.ethz.ch>

---
+++ test/cdist.py
@@ -0,0 +1,12 @@
+import cdist
+import unittest
+
+
+class CdistGeneric(unittest.TestCase):
+
+   def test_initial_manifest(self):
+      self.assertEqual(numeral, result) 
+
+
+if __name__ == '__main__':
+   unittest.main()
