099adec61fb22ca2b79005612980035326718ebd
Steven Armstrong <steven@icarus.ethz.ch>
2011-10-05 16:08:29 +0200

    start working on object class
    Signed-off-by: Steven Armstrong <steven@icarus.ethz.ch>

---
+++ lib/cdist/object.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+#
+# 2010-2011 Steven Armstrong (steven-cdist at armstrong.cc)
+#
+# This file is part of cdist.
+#
+# cdist is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# cdist is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with cdist. If not, see <http://www.gnu.org/licenses/>.
+#
+#
+
+import os
+import logging
+log = logging.getLogger(__name__)
+
+
+
+class Object(object):
+    def __init__(self, path, remote_path):
+        self.path = path
+        self.remote_path = remote_path
+
+
