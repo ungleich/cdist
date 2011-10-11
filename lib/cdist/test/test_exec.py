1c73fb1288eabb1006e67626065252e6ca0bf594
Steven Armstrong <steven@icarus.ethz.ch>
2011-10-11 16:41:01 +0200

    -- unused imports
    Signed-off-by: Steven Armstrong <steven@icarus.ethz.ch>

--- lib/cdist/test/test_exec.py
+++ lib/cdist/test/test_exec.py
@@ -22,16 +22,12 @@
 
 
 import os
-import sys
 import shutil
-import subprocess
 import tempfile
 import unittest
 
-sys.path.insert(0, os.path.abspath(
-        os.path.join(os.path.dirname(os.path.realpath(__file__)), '../lib')))
-
 import cdist.exec
+
 
 class Exec(unittest.TestCase):
     def setUp(self):
