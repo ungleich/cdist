c9ae06db6428855a3d9d849efd1019346b203c06
Nico Schottelius <nico@brief.schottelius.org>
2012-02-13 16:27:41 +0100

    do not setup __debug - let the user do if required - do not interfere type with core
    Signed-off-by: Nico Schottelius <nico@brief.schottelius.org>

--- lib/cdist/core/explorer.py
+++ lib/cdist/core/explorer.py
@@ -73,8 +73,6 @@ class Explorer(object):
             '__target_host': self.target_host,
             '__explorer': self.remote.global_explorer_path,
         }
-        if self.log.getEffectiveLevel() == logging.DEBUG:
-            self.env.update({'__debug': "yes" })
         self._type_explorers_transferred = []
 
     ### global
