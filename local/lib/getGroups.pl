ead05f940928885793604443f205c825fdff8352
carezen <github@care.com>
2012-02-27 16:43:16 -0500

    added support for non-stack hosts (just put them in the groups file somewhere)

--- local/lib/getGroups.pl
+++ local/lib/getGroups.pl
@@ -1,13 +1,18 @@
 #!/usr/bin/perl
 
 use strict;
-my $opt_debug=0;
+use Getopt::Long;
+use Data::Dumper;
+my ($debug,$listall);
+
+GetOptions("debug|d" => \$debug, "listall" => \$listall);
 
 my $groupFile="/cdist/conf/groups";
 
-if(!$ARGV[0]) { die "Usage: $0 <system>\n"; }
+if(!$ARGV[0] && !$listall) { die "Usage: $0 [--listall] <system>\n"; }
 my $system=$ARGV[0];
 my @groups;
+my %allsystems;
 
 open(IN,$groupFile) || die "Could not open $groupFile for reading\n";
 while(<IN>) {
@@ -18,12 +23,25 @@ while(<IN>) {
   foreach my $m (split(/ /,$members)) {
     debug("Checking system:$system against member:$m\n");
     if ($m eq $system) { push(@groups,$group); }
+    $allsystems{$m}=1;
   }
 }
+if($debug) { print Dumper(%allsystems); }
 
-foreach my $g (@groups) {
-  print "$g ";
+if ($listall) {
+  foreach my $s (sort keys %allsystems) {
+    print "$s ";
+  }
+  print "\n";
+  exit 0;
+} elsif (scalar @groups > 0) {
+  foreach my $g (@groups) {
+    print "$g ";
+  }
+  print "\n";
+  exit 0;
+} else {
+  exit 1;
 }
-print "\n";
 
-sub debug { print $_[0] if $opt_debug; }
+sub debug { print $_[0] if $debug; }
