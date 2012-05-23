d2fbd1565529207e9e98a5c8a023c825d670ece1
Nico Schottelius <nico@brief.schottelius.org>
2012-05-23 14:17:49 +0200

    ++version=2.0.11
    Signed-off-by: Nico Schottelius <nico@brief.schottelius.org>

--- lib/cdist/__init__.py
+++ lib/cdist/__init__.py
@@ -29,7 +29,7 @@ try:
                     'cd "%s" && git describe' % here,
                     stderr=devnull, shell=True).decode('utf-8')
 except:
-    VERSION     = "2.0.10"
+    VERSION     = "2.0.11"
 
 BANNER = """
              ..          .       .x+=:.        s
