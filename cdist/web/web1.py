a1ed7d6084fb136252f43323e4fffd10a12f574b
Nico Schottelius <nico@bento.schottelius.org>
2013-07-03 22:43:06 +0200

    add bottle examples
    Signed-off-by: Nico Schottelius <nico@bento.schottelius.org>

---
+++ cdist/web/web1.py
@@ -0,0 +1,15 @@
+import sqlite3
+from bottle import route, run, debug, template
+
+@route('/todo')
+def todo_list():
+    conn = sqlite3.connect('todo.db')
+    c = conn.cursor()
+    c.execute("SELECT id, task FROM todo WHERE status LIKE '1'")
+    result = c.fetchall()
+    c.close()
+    output = template('make_table', rows=result)
+    return str(output)
+
+debug(True)
+run(reloader=True)
