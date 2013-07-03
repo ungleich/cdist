f6a5419ad1ae72617d2b82704c119a775d0bd80e
Nico Schottelius <nico@bento.schottelius.org>
2013-07-03 23:01:24 +0200

    bottle tests
    Signed-off-by: Nico Schottelius <nico@bento.schottelius.org>

--- cdist/web/web1.py
+++ cdist/web/web1.py
@@ -1,6 +1,7 @@
 import sqlite3
-from bottle import route, run, debug, template
+from bottle import route, run, debug, template, request
 
+@route('/')
 @route('/todo')
 def todo_list():
     conn = sqlite3.connect('todo.db')
@@ -10,6 +11,51 @@ def todo_list():
     c.close()
     output = template('make_table', rows=result)
     return str(output)
+
+@route('/new', method='GET')
+def new_item():
+
+    if request.GET.get('save','').strip():
+
+        new = request.GET.get('task', '').strip()
+        conn = sqlite3.connect('todo.db')
+        c = conn.cursor()
+
+        c.execute("INSERT INTO todo (task,status) VALUES (?,?)", (new,1))
+        new_id = c.lastrowid
+
+        conn.commit()
+        c.close()
+
+        return '<p>The new task was inserted into the database, the ID is %s</p>' % new_id
+    else:
+        return template('new_task.tpl')
+
+@route('/edit/:no', method='GET')
+def edit_item(no):
+
+    if request.GET.get('save','').strip():
+        edit = request.GET.get('task','').strip()
+        status = request.GET.get('status','').strip()
+
+        if status == 'open':
+            status = 1
+        else:
+            status = 0
+
+        conn = sqlite3.connect('todo.db')
+        c = conn.cursor()
+        c.execute("UPDATE todo SET task = ?, status = ? WHERE id LIKE ?", (edit, status, no))
+        conn.commit()
+
+        return '<p>The item number %s was successfully updated</p>' % no
+    else:
+        conn = sqlite3.connect('todo.db')
+        c = conn.cursor()
+        c.execute("SELECT task FROM todo WHERE id LIKE ?", (str(no)))
+        cur_data = c.fetchone()
+
+        return template('edit_task', old=cur_data, no=no)
 
 debug(True)
 run(reloader=True)
