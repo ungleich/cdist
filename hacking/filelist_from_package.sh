648809ff444cd3716d3d8de6a78699dfa0c92ffd
Nico Schottelius <nico@wurzel.schottelius.org>
2015-05-13 10:25:49 +0200

    begin ssh integration
    Signed-off-by: Nico Schottelius <nico@wurzel.schottelius.org>

--- hacking/filelist_from_package.sh
+++ hacking/filelist_from_package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Generate filelist excluding stuff that takes only space
-for pkg in bash systemd util-linux; do
+for pkg in bash systemd util-linux openssh; do
     pacman -Qlq $pkg | grep -v \
         -e /usr/share/man/
 done
