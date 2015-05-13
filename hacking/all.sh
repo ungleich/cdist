f5edb02fbd8c3f1b00359bc5f04cb38178393c68
Nico Schottelius <nico@wurzel.schottelius.org>
2015-05-13 09:30:24 +0200

    library copy still missing
    Signed-off-by: Nico Schottelius <nico@wurzel.schottelius.org>

--- hacking/all.sh
+++ hacking/all.sh
@@ -6,5 +6,6 @@ dir=./iso
 iso=preos.iso
 
 ./filelist_from_package.sh | ./filelist_to_dir.sh "$dir"
+echo "MISSING: copy libraries" >&2
 ./add_kernel_isolinux.sh "$dir"
 ./create_iso.sh "$dir" "$iso"
