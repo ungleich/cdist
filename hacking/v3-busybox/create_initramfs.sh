7d7aa60e191a1bfa50ef45814c7e6f398c865edd
Nico Schottelius <nico@wurzel.schottelius.org>
2015-05-19 11:38:06 +0200

    create directories required by busybox
    Signed-off-by: Nico Schottelius <nico@wurzel.schottelius.org>

--- hacking/v3-busybox/create_initramfs.sh
+++ hacking/v3-busybox/create_initramfs.sh
@@ -5,14 +5,17 @@ set -ex
 initramfs_dir=$(mktemp -d /tmp/cdist-preos.XXXXXXX)
 # initramfs_dir=$1
 
-for dir in bin sbin etc proc sys newroot; do
+for dir in bin sbin etc proc sys newroot usr/bin usr/sbin; do
     mkdir -p ${initramfs_dir}/$dir
 done
 touch ${initramfs_dir}/etc/mdev.conf
 
 cp init "${initramfs_dir}/init"
 cp $(which busybox) "${initramfs_dir}/bin"
-ln -fs busybox "${initramfs_dir}/bin/sh"
+
+for link in sh mount; do
+    ln -fs busybox "${initramfs_dir}/bin/$link"
+done
 
 cd "${initramfs_dir}"
 find . | cpio -H newc -o | gzip
