ca366c877cd7fcb131c50f6eadc4a9e5efa71f7a
Darko Poljak <darko.poljak@gmail.com>
2016-07-19 07:34:21 +0200

    Add betainventory to bash/zsh completions.

--- completions/bash/cdist-completion.bash
+++ completions/bash/cdist-completion.bash
@@ -6,7 +6,7 @@ _cdist()
     prev="${COMP_WORDS[COMP_CWORD-1]}"
     prevprev="${COMP_WORDS[COMP_CWORD-2]}"
     opts="-h --help -d --debug -v --verbose -V --version"
-    cmds="banner shell config"
+    cmds="banner shell config betainventory"
 
     case "${prevprev}" in
         shell)
@@ -14,6 +14,40 @@ _cdist()
                 -s|--shell)
                     shells=$(grep -v '^#' /etc/shells)
                     COMPREPLY=( $(compgen -W "${shells}" -- ${cur}) )
+                    return 0
+                    ;;
+            esac
+            ;;
+         betainventory)
+            case "${prev}" in
+                list)
+                    opts="-h --help -d --debug -v --verbose -I --inventory \
+                        -H --host-only -a --all -t --tag -f --file"
+                    COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+                    return 0
+                    ;;
+                add-host)
+                    opts="-h --help -d --debug -v --verbose -I --inventory \
+                        -f --file"
+                    COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+                    return 0
+                    ;;
+                del-host)
+                    opts="-h --help -d --debug -v --verbose -I --inventory \
+                        -f --file -a --all"
+                    COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+                    return 0
+                    ;;
+                add-tag)
+                    opts="-h --help -d --debug -v --verbose -I --inventory \
+                        -f --file -t --taglist -T --tag-file"
+                    COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+                    return 0
+                    ;;
+                del-tag)
+                    opts="-h --help -d --debug -v --verbose -I --inventory \
+                        -f --file -t --taglist -T --tag-file -a --all"
+                    COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
                     return 0
                     ;;
             esac
@@ -36,11 +70,17 @@ _cdist()
             return 0
             ;;
         config)
-            opts="-h --help -d --debug -v --verbose \
+            opts="-h --help -d --debug -v --verbose -I --inventory \
                 -c --conf-dir -f --file -i --initial-manifest -n --dry-run \
                 -o --out-dir -p --parallel -s --sequential --remote-copy \
-                --remote-exec"
+                --remote-exec -t --tag -a --all"
             COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+            return 0
+            ;;
+        betainventory)
+            cmds="list add-host del-host add-tag del-tag"
+            opts="-h --help -d --debug -v --verbose"
+            COMPREPLY=( $(compgen -W "${opts} ${cmds}" -- ${cur}) )
             return 0
             ;;
         *)
