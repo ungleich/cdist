aac9906fff1fb356707c9c26fbac0540cae13300
Darko Poljak <darko.poljak@gmail.com>
2016-10-31 08:09:38 +0100

    Remove redundant log.debug.

--- cdist/trigger.py
+++ cdist/trigger.py
@@ -106,8 +106,6 @@ class TriggerHttp(http.server.BaseHTTPRequestHandler):
         self.do_GET()
 
     def run_cdist(self, mode, host):
-        log.debug("Running cdist for %s in mode %s", host, mode)
-
         cname = mode.title()
         module = getattr(cdist, mode)
         theclass = getattr(module, cname)
