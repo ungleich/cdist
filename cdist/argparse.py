93ccf925cde03d8d47223fe74130f7b1d29edebc
Steven Armstrong <steven@icarus.ethz.ch>
2017-01-20 15:34:17 +0100

    need a way to set remote.base_path from the command line
    Signed-off-by: Steven Armstrong <steven@icarus.ethz.ch>

--- cdist/argparse.py
+++ cdist/argparse.py
@@ -173,6 +173,10 @@ def get_parsers():
            '-s', '--sequential',
            help='operate on multiple hosts sequentially (default)',
            action='store_false', dest='parallel')
+    parser['config_args'].add_argument(
+           '-r', '--remote-out-dir',
+           help='Directory to save cdist output in on the target host',
+           dest="remote_out_path")
     parser['config'] = parser['sub'].add_parser(
             'config', parents=[parser['loglevel'], parser['beta'],
                                parser['config_main'],
