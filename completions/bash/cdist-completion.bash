a6d2407b7362b228ff15df4ebabb2978da312446
Darko Poljak <foss@ungleich.com>
2017-07-02 00:07:13 +0200

    Add -r option to completions.

--- completions/bash/cdist-completion.bash
+++ completions/bash/cdist-completion.bash
@@ -38,7 +38,7 @@ _cdist()
         config|install)
             opts="-h --help -d --debug -v --verbose -b --beta \
                 -C --cache-path-pattern -c --conf-dir -f --file -i --initial-manifest -j --jobs \
-                -n --dry-run -o --out-dir -p --parallel -s --sequential \
+                -n --dry-run -o --out-dir -p --parallel -r --remote-out-dir -s --sequential \
                 --remote-copy --remote-exec"
             COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
             return 0
